c625485a50765feb914c5ddb20b26493ba0d16d0
VijaySingh092 <vijaykhetwal372@gmail.com>
2026-07-24 19:42:01 +0530

    Restructure project into frontend and backend

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -1,10 +1,19 @@
 const jwt = require('jsonwebtoken')
+const tokenBlackListModel =require('../models/blacklist.model')
 
-function authUser(req,res,next){
+async function authUser(req,res,next){
     const token= req.cookies.token
     if(!token){
         return res.status(401).json({
             message:"Token not provided"
+        })
+    }
+
+    const isTokenBlacklisted =await tokenBlackListModel.findOne({token})
+
+    if(isTokenBlacklisted){
+        return res.status(400).json({
+            message:"Token is invalid"
         })
     }
 
