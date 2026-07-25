6adaf396d383f3e72f80ed002fd7571e1b787963
VijaySingh092 <vijaykhetwal372@gmail.com>
2026-07-25 11:51:02 +0530

    integrated Gemini api

---
+++ Backend/src/services/ai.service.js
@@ -0,0 +1,50 @@
+const {GoogleGenAI, Behavior} = require('@google/genai')
+const {z} = require('zod')
+const {zodToJsonSchema} = require('zod-to-json-schema')
+
+const ai = new GoogleGenAI({
+    apiKey:process.env.GOOGLE_GENAI_API_KEY
+})
+
+
+const interviewReportSchema = z.object({
+      matchScore: z.number().describe("A score between 0 and 100 indicating how well the candidate's profile matches the job describe"),
+    technicalQuestions: z.array(z.object({
+        question:z.string().describe('The technical question can be asked in the interview'),
+        intention:z.string().describe('The intention of interviewer behind asking this question'),
+        answer:z.string().describe('How to answer this question, what points to cover , what approach to take etc.')
+    })).describe("Technical questions that can be asked in the internview along with their intention and how to anwer them"),
+    BehavioralQuestions: z.array(z.object({
+        question:z.string().describe('The behavioral question can be asked in the interview'),
+        intention:z.string().describe('The intention of interviewer behind asking this question'),
+        answer:z.string().describe('How to answer this question, what points to cover , what approach to take etc.')
+    })).describe("Behavioral questions that can be asked in the internview along with their intention and how to anwer them"),
+    skillGaps:z.array(z.object({
+        skill:z.string().describe("The skill which the candidate is lacking"),
+        severity:z.enum(["low","medium","high"]).describe("The severity of this skill gap, i.e how important is this skill for the job and how much it can impact the candidate's chances")
+    })).describe("List of skill gaps in the candidat's profile along with their severity"),
+    preparationPlan:z.array(z.object({
+        day:z.number().describe('The day number in the preparation plan starting from 1'),
+         focus: z.string().describe("The main focus of this day in the preparation plan, e.g. data structures, system design, mock interviews etc."),
+    })).describe("A day-wise preparation plan for the candidate to follow in order to prepare for the interview effectively")
+})
+
+async function generateInterviewReport({resume,selfDescription,jobDescription}){
+
+    const prompt =`Generate an interview report for a candidate with the following details: 
+        Resume:${resume}
+        Self Description:${selfDescription}
+        Job Description:${jobDescription}`
+
+    const response = await ai.models.generateContent({
+        model:'gemini-flash-latest',
+        contents:prompt,
+        config:{
+            responseMimeType:"application/json",
+            responseSchema:zodToJsonSchema(interviewReportSchema)
+        }
+    })
+    return JSON.parse(response.text)
+}
+
+module.exports = generateInterviewReport
