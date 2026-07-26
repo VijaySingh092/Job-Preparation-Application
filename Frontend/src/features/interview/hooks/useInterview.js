f9dbddc18d2f188f1b159013c8811e45553e8850
VijaySingh092 <vijaykhetwal372@gmail.com>
2026-07-26 12:28:10 +0530

    update Home scss and recent report feature

--- Frontend/src/features/interview/hooks/useInterview.js
+++ Frontend/src/features/interview/hooks/useInterview.js
@@ -1,51 +1,68 @@
 import {getAllInterviewReports,generateInterviewReport,getInterviewReportById} from "../services/interview.api"
 import {InterviewContext} from "../interview.context"
-import { useContext } from "react"
+import { useContext,useEffect } from "react"
+import { useParams } from "react-router"
 
 
 export const useInterview = ()=>{
     const context = useContext(InterviewContext)
+    const {interviewId} = useParams()
 
     if(!context){
         throw new Error("useInterview must be used within an InterviewProvider")
     }
     const {loading,setLoading,report,setReport,reports,setReports,} =context
 
-    const generateReport = async ({jobDescription,setDescription,resumeFile})=>{
+    const generateReport = async ({jobDescription,selfDescription,resumeFile})=>{
         setLoading(true)
+        let response = null
         try{
-            const response = await generateInterviewReport({jobDescription,selfDescription,resumeFile})
-            setReport(response.interviewReport)
-        }catch(error){
-            console.log(err)
-        }finally{
-            setLoading(false)
-        }
-    }
-
-    const getReportbyId = async({interviewId})=>{
-        setLoading(true)
-        try{
-            const response = await getInterviewReportById(interviewId)
+             response = await generateInterviewReport({jobDescription,selfDescription,resumeFile})
             setReport(response.interviewReport)
         }catch(error){
             console.log(error)
         }finally{
             setLoading(false)
         }
+
+        return response.interviewReport
+    }
+
+    const getReportById = async(interviewId)=>{
+        setLoading(true)
+        let response = null
+        try{
+            response = await getInterviewReportById(interviewId)
+            setReport(response.interviewReport)
+        }catch(error){
+            console.log(error)
+        }finally{
+            setLoading(false)
+        }
+        return response.interviewReport
     }
 
     const getReports = async()=>{
         setLoading(true)
+        let response = null
         try{
-            const response = await getAllInterviewReports()
+             response = await getAllInterviewReports()
             setReports(response.interviewReports)
         }catch(error){
             console.log(error)
         }finally{
             setLoading(false)
         }
+        return response.interviewReports
     }
+
+    useEffect(()=>{
+        if(interviewId){
+            getReportById(interviewId)
+        }else{
+            getReports()
+        }
+    },[interviewId])
 
     return {loading, report, reports, generateReport, getReportById, getReports}
 }
